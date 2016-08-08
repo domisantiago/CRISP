dc2c586d3e99bf1157c67958a0bc606e0e3da26c
Santiago <SantiagD@corp.root.nasd.com>
2016-08-08 16:24:00 -0400

    Set default deployment target and always verify.

--- verify/appschema.sql
+++ verify/appschema.sql
@@ -2,6 +2,7 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+SELECT pg_catalog.has_schema_privilege('flipr', 'usage');
+--SELECT 1/COUNT(*) FROM information_schema.schemata WHERE schema_name = 'flipr';
 
 ROLLBACK;
